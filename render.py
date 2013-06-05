206227db1a77661170ac2e6a0bcfc58c90e2b887
babykick <babykick@163.com>
2013-06-05 08:38:36 +0800

    rechanged to Mako :)

--- render.py
+++ render.py
@@ -1,7 +1,12 @@
-from jinja2 import Environment, PackageLoader
+from mako.template import Template
+from mako.runtime import Context
+from StringIO import StringIO
+from mako.lookup import TemplateLookup
 
-env = Environment(loader=PackageLoader('yourapplication', 'templates'))
-template = env.get_template('mytemplate.html')
-print template.render(the='variables', go='here')
- 
+mylookup = TemplateLookup(directories=['.'])
 
+mytemplate = Template(filename="first.tpl", lookup=mylookup)
+buf = StringIO()
+ctx = Context(buf, name="jack")
+mytemplate.render_context(ctx)
+print buf.getvalue()
